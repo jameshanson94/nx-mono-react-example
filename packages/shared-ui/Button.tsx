8ce2f38b7564491d9ebd9b581f7836caadbc2427
James Hanson <james.hanson@enactor.co.uk>
2024-07-08 12:32:26 +0100

    updated button

--- packages/shared-ui/Button.tsx
+++ packages/shared-ui/Button.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 export function Button(props: any) {
-  return <button onClick={() => props.onClick()}>{props.children}</button>;
+  return <button onClick={() => props.onClick()}>V2 {props.children}</button>;
 }
 
 export default Button;
