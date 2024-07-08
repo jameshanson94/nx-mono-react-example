ba209a26c70809005933042c8bbc34c37c39ce88
James Hanson <james.hanson@enactor.co.uk>
2024-07-08 13:18:16 +0100

    react-vite-ui working

--- packages/react-vite-ui/src/ViteComponent.tsx
+++ packages/react-vite-ui/src/ViteComponent.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 export default function ViteComponent(props: any) {
   return (
     <div>
+      <h1>V1</h1>
       <h1>Hi, I'm ViteComponent</h1>
       <p>Here are my props:</p>
       <pre>{JSON.stringify(props, null, 2)}</pre>
